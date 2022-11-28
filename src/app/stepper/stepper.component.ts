a0b49e79f4b3bb4abf27207c12ad4600603519f4
omniaaaaahmed <omnia.ahmed@kortobaa.com>
2022-11-28 09:51:32 +0200

    Refactor:remove service and display static data

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,4 +1,3 @@
-import { StepperService } from './../stepper.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,14 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stepper.component.scss'],
 })
 export class StepperComponent implements OnInit {
-  arr:any[]=[];
-  constructor(private __stepperService: StepperService) {}
+  arr: any[] = [
+    {
+      postId: 1,
+      id: 1,
+      name: 'id labore ex et quam laborum',
+      email: 'Eliseo@gardner.biz',
+      body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
+    },
+    {
+      postId: 1,
+      id: 2,
+      name: 'quo vero reiciendis velit similique earum',
+      email: 'Jayne_Kuhic@sydney.com',
+      body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
+    },
+    {
+      postId: 1,
+      id: 3,
+      name: 'odio adipisci rerum aut animi',
+      email: 'Nikita@garfield.biz',
+      body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
+    },
+    {
+      postId: 1,
+      id: 4,
+      name: 'alias odio sit',
+      email: 'Lew@alysha.tv',
+      body: 'non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati',
+    },
+    {
+      postId: 1,
+      id: 5,
+      name: 'vero eaque aliquid doloribus et culpa',
+      email: 'Hayden@althea.biz',
+      body: 'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et',
+    },
+  ];
+  constructor() {}
 
   ngOnInit(): void {
-    this.__stepperService.getAllComments().subscribe((res) => {
-      console.log(res);
-      this.arr = res
-      this.arr.sort((a,b) => b.id - a.id);
-    });
+    this.arr.sort((a, b) => b.id - a.id);
+
   }
 }
